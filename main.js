724db1c9314faf0c86007c56905f6ce100ff0510
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-07-31 15:41:54 +0200

    Remove DevTools and window frame

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ function main() {
     webPreferences: {
       nodeIntegration: true
     },
-    width: 800,
-    height: 600,
+    width: 520,
+    height: 650,
+    frame: false,
   })
 
   // load app/index.html as the window content
   mainWindow.loadFile(path.join('app', 'index.html'));
-  mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', main);
